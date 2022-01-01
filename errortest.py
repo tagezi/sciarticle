39e585b058c420b547987dff85e81ee87b1c957f
tagezi <lera.goncharuk@gmail.com>
2022-01-01 16:15:44 +0300

    Aligning the code to the license rules

--- errortest.py
+++ errortest.py
@@ -1,6 +1,23 @@
+#     This code is a part of program Science Jpurnal
+#     Copyright (C) 2021  Valerii Goncharuk (aka tagezi)
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+
 
 def getTeg(url):
     
@@ -14,6 +31,7 @@ def getTeg(url):
     except AttributeError as e:
         return None
     return titel
+
 
 titel = getTeg("https://virtualenv.pypa.io/en/latest/installation.html")
 if titel == None:
--- sqlmain.py
+++ sqlmain.py
@@ -1,9 +1,24 @@
+#     This code is a part of program Science Jpurnal
+#     Copyright (C) 2021  Valerii Goncharuk (aka tagezi)
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import sqlite3
 from sqlite3 import DatabaseError
 
 
 # Provides a simple interface for working with a database with others scripts.
-
 class Sqlmain():
     def __init__(self, sFileDB):
         self.oConnect = sqlite3.connect(sFileDB)
