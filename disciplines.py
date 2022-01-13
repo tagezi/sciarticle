797baea3964ca03038927beda4dfd5742ecb6122
tagezi <lera.goncharuk@gmail.com>
2022-01-13 12:14:30 +0300

    Documenting code

--- disciplines.py
+++ disciplines.py
@@ -13,19 +13,33 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+""" Fills database by discipline of science journals and books. It takes little
+    more garbage than can be hope (See: TODO)
+    """
 from lib.sqlmain import *
 from lib.strmain import *
 from lib.perfect_soup import *
 import config
 
 
+# TODO: Parsing of Headers collects string without since for dial
+def get_dspln_from_h(sHeader):
+    sName = clean_spaces(sHeader.get_text()).replace('[edit]', '').lower()
+    sLink = ''
+    if sHeader.find('href') != -1 and\
+            sHeader.find('http') == -1 and sHeader.find('redlink') != -1:
+        sLink = sHeader.get_text()
+    if sName.find('outline') == -1 \
+            and not oConnector.q_get_id_dspln(sName):
+        oConnector.q_insert_dspln((sName, sLink,))
+
+
 def get_discipline():
-    sWikiPages = config.dspln_link
-    db_file = get_file_patch(config.db_dir, config.db_file)
-    oConnector = Sqlmain(db_file)
+    """ Fills database by discipline of science journals and books. """
     bsObj = PerfectSoup(sWikiPages)
+    # Take blocks where link lists are.
     lBlock = bsObj.findAll('div', {'class': 'div-col'})
+    # TODO: It collects redlink to, but them don't need
     for sBlock in lBlock:
         lTagA = sBlock.findAll('a')
         for sTagA in lTagA:
@@ -37,24 +51,15 @@ def get_discipline():
 
     lHeader2 = bsObj.findAll('h2')
     for sHeader2 in lHeader2:
-        sName = clean_spaces(sHeader2.get_text()).replace('[edit]', '').lower()
-        sLink = ''
-        if sHeader2.find('href') != -1 and sHeader2.find('http') == -1:
-            sLink = sHeader2.get_text()
-        if sName.find('outline') == -1 \
-                and not oConnector.q_get_id_dspln(sName):
-            oConnector.q_insert_dspln((sName, sLink,))
+        get_dspln_from_h(sHeader2)
 
     lHeader3 = bsObj.findAll('h3')
     for sHeader3 in lHeader3:
-        sName = clean_spaces(sHeader3.get_text()).replace('[edit]', '').lower()
-        sLink = ''
-        if sHeader3.find('href') != -1 and sHeader3.find('http') == -1:
-            sLink = clean_spaces(sHeader3.get_text())
-        if sName.find('outline') == -1 \
-                and not oConnector.q_get_id_dspln(sName):
-            oConnector.q_insert_dspln((sName, sLink,))
+        get_dspln_from_h(sHeader3)
 
 
 if __name__ == '__main__':
+    sWikiPages = config.dspln_link
+    db_file = get_file_patch(config.db_dir, config.db_file)
+    oConnector = Sqlmain(db_file)
     get_discipline()
