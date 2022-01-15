7b140e3f32ac4f582165db1a1a712ce69a3da13b
tagezi <lera.goncharuk@gmail.com>
2022-01-15 18:53:32 +0300

    Tuple begins with 't' now

--- lib/sqlmain.py
+++ lib/sqlmain.py
@@ -110,60 +110,60 @@ class SQLmain():
 
         return True
 
-    def execute_query(self, sqlString, cValues=None):
+    def execute_query(self, sqlString, tValues=None):
         """ Method executes sql script.
 
         :param sqlString: SQL query as string.
-        :param cValues: value(s) that need to safe inserting into query
+        :param tValues: value(s) that need to safe inserting into query
                         (by default, None).
         :return: True if script execution is successful, otherwise False.
         """
         oCursor = self.oConnect.cursor()
         try:
-            if cValues is None:
+            if tValues is None:
                 oCursor.execute(sqlString)
             else:
-                oCursor.execute(sqlString, cValues)
+                oCursor.execute(sqlString, tValues)
         except DatabaseError as e:
             logging.exception('An error has occurred: %s.\n'
                               'String of query: %s \n'
-                              'Parameters^ %s', e, sqlString, cValues)
+                              'Parameters^ %s', e, sqlString, tValues)
             return False
 
         return oCursor
 
-    def insert_row(self, sTable, sColumns, cValues):
+    def insert_row(self, sTable, sColumns, tValues):
         """ Inserts a record in the database table.
 
         :param sTable: Table name as string.
         :param sColumns: Columns names of the table by where needs inserting.
-        :param cValues: Value(s) as tuple for inserting.
+        :param tValues: Value(s) as tuple for inserting.
         :return: True if the insert was successful, otherwise False.
         """
         sSQL = ("?, " * len(sColumns.split(", ")))[:-2]
         sqlString = "INSERT INTO " \
                     + sTable + " (" + sColumns + ") VALUES (" + sSQL + ") "
-        oCursor = self.execute_query(sqlString, cValues)
+        oCursor = self.execute_query(sqlString, tValues)
         if not oCursor:
             return False
 
         self.oConnect.commit()
         return True
 
-    def delete_row(self, sTable, sColumns=None, cValues=None):
+    def delete_row(self, sTable, sColumns=None, tValues=None):
         """ Deletes row in the database table by value(s).
 
         :param sTable: A table as string in where need to delete row.
         :param sColumns: Column(s) where the value(s) will be found.
                          (by default, None).
-        :param cValues: value(s) as tuple for search of rows.
+        :param tValues: value(s) as tuple for search of rows.
                         (by default, None).
         :return: True if the deletion is successful, otherwise False.
         """
         if sColumns is not None:
             sqlString = 'DELETE FROM ' + sTable + ' WHERE ' + \
                         get_columns(sColumns)
-            oCursor = self.execute_query(sqlString, cValues)
+            oCursor = self.execute_query(sqlString, tValues)
         else:
             sqlString = "DELETE FROM " + sTable
             oCursor = self.execute_query(sqlString)
@@ -174,34 +174,34 @@ class SQLmain():
         self.oConnect.commit()
         return True
 
-    def update(self, sTable, sSetUpdate, sWhereUpdate, cValues):
+    def update(self, sTable, sSetUpdate, sWhereUpdate, tValues):
         """ Updates value(s) in the record of the database table.
 
         :param sTable: A Table as string where update is need to do.
         :param sSetUpdate: Column(s) where the value are writen.
         :param sWhereUpdate: A column where values correspond to the required.
-        :param cValues: value(s) as tuple for search corresponding rows.
+        :param tValues: value(s) as tuple for search corresponding rows.
         :return: True if the insert was successful, otherwise False.
         """
         sSetUpdate = sSetUpdate + "=?"
         sWhereUpdate = get_columns(sWhereUpdate)
         sqlString = "UPDATE " + sTable + " SET " + sSetUpdate + \
                     " WHERE " + sWhereUpdate + " "
-        oCursor = self.execute_query(sqlString, cValues)
+        oCursor = self.execute_query(sqlString, tValues)
         if not oCursor:
             return False
 
         self.oConnect.commit()
         return True
 
-    def select(self, sTable, sGet, sWhere=None, cValues=None, sFunc=None):
+    def select(self, sTable, sGet, sWhere=None, tValues=None, sFunc=None):
         """ Looks for row by value(s) in table column(s).
 
         :param sTable: Table name as string.
         :param sGet: Name of the column of the table, which will be returned.
         :param sWhere: Names of columns of the table, by which to search
                        (by default, None).
-        :param cValues: Value(s) as tuple for search
+        :param tValues: Value(s) as tuple for search
                         (by default, None).
         :param sFunc: Function name of sqlite, which need to apply
                       (by default, None). Now, you can use only two sqlite
@@ -217,7 +217,7 @@ class SQLmain():
         if sWhere is not None:
             sCol = get_columns(sWhere)
             sqlString = "SELECT " + sGet + " FROM " + sTable + " WHERE " + sCol
-            oCursor = self.execute_query(sqlString, cValues)
+            oCursor = self.execute_query(sqlString, tValues)
         else:
             oCursor = self.execute_query("SELECT " + sGet + " FROM " + sTable)
         if not oCursor:
@@ -226,18 +226,18 @@ class SQLmain():
         return oCursor
 
     # Average API level
-    def sql_get_id(self, sTable, sID, sColumns, cValues):
+    def sql_get_id(self, sTable, sID, sColumns, tValues):
         """ Looks for ID of the row by value(s) of table column(s).
 
         :param sTable: Table name as string.
         :param sID: Name of the column of the table by which to search.
         :param sColumns: Names of columns of the table by which to search.
-        :param cValues: Value(s) as tuple for search.
+        :param tValues: Value(s) as tuple for search.
         :return: ID as Number in the row cell, or 0, if the row not found.
         """
         sCol = get_columns(sColumns)
         sqlString = "SELECT " + sID + " FROM " + sTable + " WHERE " + sCol
-        oCursor = self.execute_query(sqlString, cValues)
+        oCursor = self.execute_query(sqlString, tValues)
         if not oCursor:
             return False
         else:
@@ -266,7 +266,7 @@ class SQLmain():
         :param sTable: Table name as string where records should be count.
         :return: Number of found records.
         """
-        # sTable, sGet, sWhere, cValues, sFunc=None
+        # sTable, sGet, sWhere, tValues, sFunc=None
         oCursor = self.select(sTable, '*', None, None, 'Count')
         if not oCursor:
             return False
@@ -346,135 +346,135 @@ class SQLmain():
                                'publisher_name', (sValue,))
 
     # dspln is accepted abbreviation of word 'discipline'
-    def q_insert_book_dspln(self, cValues):
+    def q_insert_book_dspln(self, tValues):
         """ Inserts values into BookDiscipline table.
 
-            :param cValues: Values which need to insert. This parameter
+            :param tValues: Values which need to insert. This parameter
                 should contain 2 values: either 2 int, or 1 int and 1 str,
                 or 2 str. The first from them should be values of book,
                 and the second is discipline.
                 Otherwise, an exception will be thrown.
             :return: True, if inserting is successful. Otherwise, False is.
         """
-        if type(cValues[0]) == int and type(cValues[1]) == str:
-            iIDDspln = self.q_get_id_dspln(cValues[1])
+        if type(tValues[0]) == int and type(tValues[1]) == str:
+            iIDDspln = self.q_get_id_dspln(tValues[1])
             if not iIDDspln:
                 raise NameError('The discipline value %s return bool (%s)'
-                                ' value', cValues[1], iIDDspln)
-            cValues = (cValues[0], iIDDspln,)
+                                ' value', tValues[1], iIDDspln)
+            tValues = (tValues[0], iIDDspln,)
 
-        elif type(cValues[0]) == str:
-            iIDBook = self.q_get_id_book(cValues[0])
+        elif type(tValues[0]) == str:
+            iIDBook = self.q_get_id_book(tValues[0])
             if not iIDBook:
                 raise NameError('The book values %s, %s, %s return bool (%s)'
-                                ' value', cValues[0], iIDBook)
+                                ' value', tValues[0], iIDBook)
 
-            if type(cValues[1]) == str:
-                iIDDspln = self.q_get_id_dspln(cValues[1])
+            if type(tValues[1]) == str:
+                iIDDspln = self.q_get_id_dspln(tValues[1])
                 if not iIDDspln:
                     raise NameError('The discipline value %s return bool (%s)'
-                                    ' value', cValues[1], iIDDspln)
-                cValues = (iIDBook, iIDDspln,)
+                                    ' value', tValues[1], iIDDspln)
+                tValues = (iIDBook, iIDDspln,)
             else:
-                cValues = (iIDBook, cValues[1],)
+                tValues = (iIDBook, tValues[1],)
         sColumns = 'id_book, id_discipline'
 
-        return self.insert_row('BookDiscipline', sColumns, cValues)
+        return self.insert_row('BookDiscipline', sColumns, tValues)
 
-    def q_insert_book_editor(self, lValues):
+    def q_insert_book_editor(self, tValues):
         """ Inserts values into Editor table.
 
-            :param lValues: Values which need to insert. This parameter should
+            :param tValues: Values which need to insert. This parameter should
                             contain 2 values, otherwise will be call exception.
             :return: True if inserting is successful, otherwise False.
             """
-        return self.insert_row('BookEditor', 'id_book, editor', lValues)
+        return self.insert_row('BookEditor', 'id_book, editor', tValues)
 
-    def q_insert_book_lang(self, cValues):
+    def q_insert_book_lang(self, tValues):
         """ Inserts values into BookLang table.
 
-            :param cValues: Values which need to insert. This parameter
+            :param tValues: Values which need to insert. This parameter
                 should contain 2 values: either 2 int, or 1 int and 1 str,
                 or 2 str. The first from them should be values of book,
                 and the second is Lang.
                 Otherwise, an exception will be thrown.
             :return: True if inserting is successful, otherwise False.
             """
-        if type(cValues[0]) == int and type(cValues[1]) == str:
-            iIDLang = self.q_get_id_lang_by_name(cValues[1])
+        if type(tValues[0]) == int and type(tValues[1]) == str:
+            iIDLang = self.q_get_id_lang_by_name(tValues[1])
             if not iIDLang:
                 raise NameError('The discipline value %s return bool (%s)'
-                                ' value', cValues[1], iIDLang)
-            cValues = (cValues[0], iIDLang,)
+                                ' value', tValues[1], iIDLang)
+            tValues = (tValues[0], iIDLang,)
 
-        elif type(cValues[0]) == str:
-            iIDBook = self.q_get_id_book(cValues[0])
+        elif type(tValues[0]) == str:
+            iIDBook = self.q_get_id_book(tValues[0])
             if not iIDBook:
                 raise NameError('The book values %s, %s, %s return bool (%s)'
-                                ' value', cValues[0], iIDBook)
+                                ' value', tValues[0], iIDBook)
 
-            if type(cValues[1]) == str:
-                iIDLang = self.q_get_id_lang_by_name(cValues[1])
+            if type(tValues[1]) == str:
+                iIDLang = self.q_get_id_lang_by_name(tValues[1])
                 if not iIDLang:
                     raise NameError('The discipline value %s return bool (%s)'
-                                    ' value', cValues[1], iIDLang)
-                cValues = (iIDBook, iIDLang,)
+                                    ' value', tValues[1], iIDLang)
+                tValues = (iIDBook, iIDLang,)
             else:
-                cValues = (iIDBook, cValues[1],)
+                tValues = (iIDBook, tValues[1],)
 
-        return self.insert_row('BookLang', 'id_book, id_lang', cValues)
+        return self.insert_row('BookLang', 'id_book, id_lang', tValues)
 
-    def q_insert_dspln(self, lValues):
+    def q_insert_dspln(self, tValues):
         """ Inserts values into Discipline table.
 
-            :param lValues: Values which need to insert. This parameter should
+            :param tValues: Values which need to insert. This parameter should
                             contain 1 values, otherwise will be call exception.
             :return: True if inserting is successful, otherwise False.
             """
         return self.insert_row('Discipline',
-                               'discipline_name, discipline_url', lValues)
+                               'discipline_name, discipline_url', tValues)
 
-    def q_insert_lang(self, cValues):
+    def q_insert_lang(self, tValues):
         """ Inserts values into Lang table.
 
-            :param cValues: Values which need to insert. This parameter should
+            :param tValues: Values which need to insert. This parameter should
                             contain 8 values, otherwise will be call exception.
             :return: True if inserting is successful, otherwise False.
             """
         lValues = []
-        if len(cValues) < 8:
-            for Value in cValues:
+        if len(tValues) < 8:
+            for Value in tValues:
                 lValues.append(Value)
             while len(lValues) < 8:
                 lValues.append('')
-            cValues = tuple(lValues)
+            tValues = tuple(lValues)
         sColumns = "lang, iso_639_1, iso_639_2, iso_639_3, " \
                    "iso_639_5, gost_7_75_lat, gost_7_75_rus, d_code "
-        return self.insert_row('Lang', sColumns, cValues)
+        return self.insert_row('Lang', sColumns, tValues)
 
-    def q_insert_lang_var(self, cValues):
+    def q_insert_lang_var(self, tValues):
         """ Inserts values into LangVariant table.
 
-            :param cValues: Values which need to insert.This parameter should
+            :param tValues: Values which need to insert.This parameter should
                             contain 2 values, otherwise will be call exception.
             :return: True if inserting is successful, otherwise False.
             """
-        return self.insert_row('LangVariant', 'id_lang, lang', cValues)
+        return self.insert_row('LangVariant', 'id_lang, lang', tValues)
 
-    def q_update_book(self, sSetUpdate, lValues):
+    def q_update_book(self, sSetUpdate, tValues):
         """ Update values into Book table by id_book.
 
             :param sSetUpdate: Column(s) where the value are needed to write
-            :param lValues: value(s) as tuple for search corresponding rows.
+            :param tValues: value(s) as tuple for search corresponding rows.
             :return: True if the insert was successful, otherwise False.
             """
-        return self.update('Book', sSetUpdate, 'id_book', lValues)
+        return self.update('Book', sSetUpdate, 'id_book', tValues)
 
-    def q_update_publisher(self, sSetUpdate, lValues):
+    def q_update_publisher(self, sSetUpdate, tValues):
         """ Update values into Publisher table.
 
             :param sSetUpdate: Column(s) where the value are needed to write
-            :param lValues: value(s) as tuple for search corresponding rows.
+            :param tValues: value(s) as tuple for search corresponding rows.
             :return: True if the insert was successful, otherwise False.
             """
-        return self.update('Publisher', sSetUpdate, 'id_publisher', lValues)
+        return self.update('Publisher', sSetUpdate, 'id_publisher', tValues)
